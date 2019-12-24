7996cb8c197177846c485f6e2fb086ba84b0270a
云却 <479813005@qq.com>
2019-12-24 21:49:01 +0800

    添加一个通用接口

---
+++ Megumin.Explosion/Megumin/Interface/IGetByKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Megumin
+{
+    public interface IGetByKey<in Key, in Value>
+    {
+        bool TryGet<T, V>(T key, out V value) where T : Key where V : Value;
+    }
+}
