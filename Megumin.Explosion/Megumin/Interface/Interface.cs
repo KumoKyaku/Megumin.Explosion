1cdaf300f39411e9b07d1ea35fdba3d759c8b0d0
云却 <479813005@qq.com>
2020-01-16 00:47:32 +0800

    增加插槽节点接口

--- Megumin.Explosion/Megumin/Interface/Interface.cs
+++ Megumin.Explosion/Megumin/Interface/Interface.cs
@@ -13,4 +13,44 @@ namespace Megumin
     {
         bool Match(T input, K target);
     }
+
+    /// <summary>
+    /// 节点
+    /// </summary>
+    public interface INode
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="V"></typeparam>
+    public interface INode<V>:INode
+    {
+        V NodeValue { get; set; }
+    }
+
+    /// <summary>
+    /// 插槽
+    /// </summary>
+    public interface ISlot
+    {
+
+    }
+
+    public interface ISlot<V>
+    {
+        V SlotValue { get; set; }
+    }
+
+    /// <summary>
+    /// 插槽
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public interface ISlot<K, V>:IEnumerable<KeyValuePair<K,V>>
+    {
+        V this[K key] { get;set; }
+    }
 }
