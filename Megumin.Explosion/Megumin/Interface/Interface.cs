d9a90bc4e637da7162c7eb4706958ac267998da0
云却 <479813005@qq.com>
2020-01-20 00:25:23 +0800

    增加一组游戏用接口

--- Megumin.Explosion/Megumin/Interface/Interface.cs
+++ Megumin.Explosion/Megumin/Interface/Interface.cs
@@ -15,6 +15,14 @@ namespace Megumin
     }
 
     /// <summary>
+    /// 路径
+    /// </summary>
+    public interface IPath
+    {
+
+    }
+
+    /// <summary>
     /// 节点
     /// </summary>
     public interface INode
@@ -52,5 +60,98 @@ namespace Megumin
     public interface ISlot<K, V>:IEnumerable<KeyValuePair<K,V>>
     {
         V this[K key] { get;set; }
+    }
+
+    public interface IVisible
+    {
+        bool Visible { get; set; }
+    }
+
+    public interface IOpenable
+    {
+        void Open();
+    }
+
+    public interface ICloseable
+    {
+        void Close();
+    }
+
+    public interface IRefreshable
+    {
+        void Refresh();
+    }
+
+    public interface IDirtyable
+    {
+        bool Dirty { get; set; }
+    }
+
+    public interface IPauseable
+    {
+        bool Pause { get; set; }
+    }
+
+    public interface ITickable
+    {
+        void Tick(double delta);
+    }
+}
+
+namespace Megumin
+{
+    public interface IAwakeable
+    {
+        void Awake();
+    }
+
+    public interface IInitable
+    {
+        void Init();
+    }
+
+    public interface IEnable
+    {
+        bool Enable { get; set; }
+    }
+
+    public interface IResetable
+    {
+        void Reset();
+    }
+
+    public interface IStartable
+    {
+        void Start();
+    }
+
+    public interface IFixedUpdateable
+    {
+        void FixedUpdate();
+    }
+
+    public interface IUpdateable
+    {
+        void Update();
+    }
+
+    public interface ILateUpdateable
+    {
+        void LateUpdate();
+    }
+
+    public interface IDestroyable
+    {
+        void Destroy();
+    }
+
+    public interface IQuitable
+    {
+        void Quit();
+    }
+
+    public interface IExitable
+    {
+        void Exit();
     }
 }
