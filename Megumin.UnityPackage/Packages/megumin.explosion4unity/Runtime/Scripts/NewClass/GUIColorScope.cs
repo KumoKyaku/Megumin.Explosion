ca9594e959368aec69160382b35c9268973d1b78
云却 <479813005@qq.com>
2020-09-07 21:47:37 +0800

    增加一个颜色域

---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/GUIColorScope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// gui颜色区域
+    /// </summary>
+    public struct GUIColorScope : IDisposable
+    {
+        Color old;
+        public GUIColorScope(Color color)
+        {
+#if UNITY_EDITOR
+            old = GUI.color;
+            GUI.color = color;
+#endif
+        }
+
+        public void Dispose()
+        {
+#if UNITY_EDITOR
+            GUI.color = old;
+#endif
+        }
+    }
+}
