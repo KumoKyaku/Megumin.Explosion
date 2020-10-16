df3a6d124a34e2a52561d8a0596bbeadc82f7c46
云却 <479813005@qq.com>
2020-10-16 22:36:53 +0800

    增加一个缓冲队列，增加引用dll

---
+++ Megumin.Explosion/Megumin/Interface/IPipeQueue.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Megumin
+{
+    /// <summary>
+    /// 管道队列，用于处理生产者已经产出，消费者还没有就绪的情况
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IPipeQueue<T>
+    {
+        bool UseSynchronizationContext { get; set; }
+        /// <summary>
+        /// 当队列中有元素时返回。
+        /// </summary>
+        /// <returns></returns>
+        ValueTask<T> ReadAsync();
+        void Write(T item);
+    }
+}
