df7a1409d599a701353e42cf703543e992ee9a05
云却 <479813005@qq.com>
2020-12-30 11:15:10 +0800

    重命名一个特性

--- Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/ReadOnlyInInspectorAttribute.cs
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/ReadOnlyInInspectorAttribute.cs
@@ -4,14 +4,14 @@
     /// 让字段在编辑器中只读，防止被错误的修改
     /// </summary>
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public sealed class LockedInInspectorAttribute : PropertyAttribute
+    public sealed class ReadOnlyInInspectorAttribute : PropertyAttribute
     {
 
     }
 
 #if UNITY_EDITOR
 
-    [UnityEditor.CustomPropertyDrawer(typeof(LockedInInspectorAttribute))]
+    [UnityEditor.CustomPropertyDrawer(typeof(ReadOnlyInInspectorAttribute))]
     public class LockedInInspectorAttributeDrawer : UnityEditor.PropertyDrawer
     {
         public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
