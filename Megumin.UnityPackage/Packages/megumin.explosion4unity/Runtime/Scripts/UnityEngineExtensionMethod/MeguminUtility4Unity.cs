d7dafa656edd8e61e4d85b6ef167b91513e56053
云却 <479813005@qq.com>
2021-01-11 10:49:09 +0800

    增加一个unity路径

---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/UnityEngineExtensionMethod/MeguminUtility4Unity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityEngine
+{
+    public class MeguminUtility4Unity
+    {
+        static string packagePath = "";
+        public static string PackagesPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(packagePath))
+                {
+                    packagePath = Path.Combine(Application.dataPath, "..\\Packages");
+                    packagePath = Path.GetFullPath(packagePath);
+                }
+                return packagePath;
+            }
+        }
+    }
+}
