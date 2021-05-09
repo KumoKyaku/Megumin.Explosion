f44d239eea2e2662747002a40fc2d5f56246d75f
云却 <479813005@qq.com>
2021-05-09 14:27:40 +0800

    更新链接和目录

--- Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/DirectoryUtility.cs
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/DirectoryUtility.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using static System.Environment;
 
 namespace Megumin
 {
     internal static class DirectoryUtility
     {
+        [MenuItem("Tools/Path/Log Environment SpecialFolder")]
+        static void LogEnvironmentSpecialFolder()
+        {
+            foreach (SpecialFolder item in System.Enum.GetValues(typeof(SpecialFolder)))
+            {
+                Debug.Log($"{item.ToString().Html(HexColor.Azure)}:    {GetFolderPath(item)}");
+            }
+        }
+
         [MenuItem("Tools/Path/Create Build Folder")]
         static void CreateBuildFolder()
         {
@@ -33,5 +43,17 @@ namespace Megumin
             Debug.Log($"打开 {dir}");
             System.Diagnostics.Process.Start(dir);
         }
+
+        [MenuItem("Tools/Path/Open AssetStoreCache Folder")]
+        static void OpenAssetStoreCacheFolder()
+        {
+            var dir = Path.Combine(GetFolderPath(SpecialFolder.ApplicationData),
+                                   "Unity/Asset Store-5.x");
+            dir = Path.GetFullPath(dir);
+            Debug.Log($"打开 {dir}");
+            System.Diagnostics.Process.Start(dir);
+        }
+
+
     }
 }
--- Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/Link.cs
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/Link.cs
@@ -7,61 +7,74 @@ namespace Megumin
 {
     internal class Link
     {
+        static void OpenURL(string url)
+        {
+            Debug.Log($"打开链接：{url}");
+            Application.OpenURL(url);
+        }
+
+
         [MenuItem("Tools/Link/Unity/ImmediateWindow")]
         static void ImmediateWindow()
         {
-            Application.OpenURL("https://github.com/Unity-Technologies/com.unity.immediate-window");
+            OpenURL("https://github.com/Unity-Technologies/com.unity.immediate-window");
         }
 
 
         [MenuItem("Tools/Link/Unity-UI-Extensions")]
         static void Unity_UI_Extensions()
         {
-            Application.OpenURL("https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/src/release/");
+            OpenURL("https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/src/release/");
         }
 
 
         [MenuItem("Tools/Link/IconList")]
         static void IconList()
         {
-            Application.OpenURL("https://github.com/halak/unity-editor-icons/blob/master/README.md");
+            OpenURL("https://github.com/halak/unity-editor-icons/blob/master/README.md");
         }
 
         [MenuItem("Tools/Link/群友分享链接")]
         static void LinkList()
         {
-            Application.OpenURL("https://docs.qq.com/sheet/DUnhtZVRBR05xTXVI?tab=BB08J2");
+            OpenURL("https://docs.qq.com/sheet/DUnhtZVRBR05xTXVI?tab=BB08J2");
         }
 
         [MenuItem("Tools/Link/UniRx")]
         static void UniRx()
         {
-            Application.OpenURL("https://github.com/neuecc/UniRx#upm-package");
+            OpenURL("https://github.com/neuecc/UniRx#upm-package");
         }
 
         [MenuItem("Tools/Link/UniTask")]
         static void UniTask()
         {
-            Application.OpenURL("https://github.com/Cysharp/UniTask#upm-package");
+            OpenURL("https://github.com/Cysharp/UniTask#upm-package");
         }
 
         [MenuItem("Tools/Link/LINQ-to-GameObject-for-Unity")]
         static void LINQtoGameObjectforUnity()
         {
-            Application.OpenURL("https://github.com/neuecc/LINQ-to-GameObject-for-Unity/#linq-to-gameobject");
+            OpenURL("https://github.com/neuecc/LINQ-to-GameObject-for-Unity/#linq-to-gameobject");
         }
 
         [MenuItem("Tools/Link/Newtonsoft.Json for Unity")]
         static void NewtonsoftJson()
         {
-            Application.OpenURL("https://github.com/jilleJr/Newtonsoft.Json-for-Unity#installation-via-git-in-upm");
+            OpenURL("https://github.com/jilleJr/Newtonsoft.Json-for-Unity#installation-via-git-in-upm");
         }
 
 
         [MenuItem("Tools/Link/ClassTypeReference for Unity")]
         static void ClassTypeReference()
         {
-            Application.OpenURL("https://github.com/SolidAlloy/ClassTypeReference-for-Unity");
+            OpenURL("https://github.com/SolidAlloy/ClassTypeReference-for-Unity");
+        }
+
+        [MenuItem("Tools/Link/ShaderGraphNode/3D Nosie")]
+        static void JimmyCushnie_Noisy_Nodes()
+        {
+            OpenURL("https://github.com/JimmyCushnie/Noisy-Nodes");
         }
     }
 }
