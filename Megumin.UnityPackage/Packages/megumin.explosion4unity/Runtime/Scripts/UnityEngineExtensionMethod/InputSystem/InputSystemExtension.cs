313b674ec2a5e145153a09d7a677a0864123223c
云却 <479813005@qq.com>
2021-05-11 17:08:36 +0800

    增加扩展

---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/UnityEngineExtensionMethod/InputSystem/InputSystemExtension.cs
@@ -0,0 +1,34 @@
+#if ENABLE_INPUT_SYSTEM
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.InputSystem
+{
+    public static class InputSystemExtension_F4571CE6
+    {
+        public static string ToLogStr(this in InputAction.CallbackContext context)
+        {
+            return $"frameID:{Time.frameCount}, Context:{{started:{context.started}, performed:{context.performed}, canceled:{context.canceled}, Value:{context.ReadValueAsObject()}, action:{context.action}}}";
+        }
+
+        public static void Log(this in InputAction.CallbackContext context)
+        {
+            Debug.Log(context.ToLogStr());
+        }
+
+        public static void LogError(this in InputAction.CallbackContext context)
+        {
+            Debug.LogError(context.ToLogStr());
+        }
+    }
+}
+
+
+#endif
+
+
+
+
+
