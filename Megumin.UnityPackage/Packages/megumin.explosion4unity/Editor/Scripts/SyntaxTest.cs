4b0f143fab82ac2546b911536f8a993151c0b1c0
云却 <479813005@qq.com>
2021-10-15 06:51:32 +0800

    增加语法测试

---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/SyntaxTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Megumin.Syntax
+{
+    public interface ITestInterface<T>
+    {
+        public bool Fun(T v)
+        {
+            return default(bool);
+        }
+    }
+
+    public interface ITestInterface2 : ITestInterface<int>
+    {
+
+    }
+
+    public class SyntaxTest
+    {
+        /// <summary>
+        /// 默认接口实现与泛型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        [UnityEngine.Scripting.Preserve]
+        public void Test<T>(T t)
+            where T : ITestInterface2
+        {
+            t.Fun(1);
+        }
+    }
+}
+
