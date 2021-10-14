d49f86a43ddaff0ce371998e842dcc3f955733e3
云却 <479813005@qq.com>
2021-10-15 07:36:07 +0800

    增加语法测试,装箱分析

--- Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/SyntaxTest.cs
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/SyntaxTest.cs
@@ -6,7 +6,7 @@ namespace Megumin.SyntaxTest
 {
     public interface ITestInterface<T>
     {
-        public bool Fun(T v)
+        public bool Foo(T v)
         {
             return default(bool);
         }
@@ -27,18 +27,19 @@ namespace Megumin.SyntaxTest
         public void Test<T>(T t)
             where T : ITestInterface2
         {
-            t.Fun(1);
+            t.Foo(1);
         }
 
         public void Test2(TestStruct test)
         {
-            //会不会导致装箱?
+            //调用方法Foo 会不会导致装箱?
+            //根据指令文档猜测: 会不会装箱取决于方法的具体实现位置. 如果方法(Foo) 是在结构体实现的,不会装箱/是接口默认实现的,会装箱.
             //会. https://docs.microsoft.com/zh-cn/dotnet/api/system.reflection.emit.opcodes.constrained?view=net-5.0
             Test(test);
         }
 
         [EditorButton]
-        public void Test3() => Test2(default);
+        public void Test() => Test2(default);
     }
 }
 
