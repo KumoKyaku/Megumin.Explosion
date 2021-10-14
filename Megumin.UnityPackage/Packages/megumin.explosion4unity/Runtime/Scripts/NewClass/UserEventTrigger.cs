15404fe9d1b7de5d0f90f84a9cee42b4ccce4c97
云却 <479813005@qq.com>
2021-10-14 09:54:39 +0800

    双击回调

---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/UserEventTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
+namespace Megumin
+{
+    public class UserEventTrigger : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        public float delta = 0.5f;
+        public UnityEvent OnDoubleClick;
+        float last = -1;
+        public void OnPointerDown(PointerEventData eventData)
+        {
+
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            //Debug.Log($"count {eventData.clickCount}");
+
+            var now = eventData.clickTime;
+            if (now - last < delta)
+            {
+                last = -1;
+                //双击
+                //Debug.Log("shuangji");
+                OnDoubleClick?.Invoke();
+            }
+            else
+            {
+                last = now;
+            }
+        }
+    }
+}
+
+
