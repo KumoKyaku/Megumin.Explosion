1a4857a4da47d252f810a00690b457e8f87a854d
云却 <479813005@qq.com>
2021-10-23 08:57:07 +0800

    打印函数扩展

--- Megumin.Explosion/Megumin/Class/MeguminUtility.cs
+++ Megumin.Explosion/Megumin/Class/MeguminUtility.cs
@@ -8,6 +8,7 @@ namespace Megumin
     /// <summary>
     /// 常用工具
     /// </summary>
+    [Obsolete("Use Megumin.Utility instead.")]
     public class MeguminUtility
     {
         /// <summary>
@@ -16,53 +17,8 @@ namespace Megumin
         /// <typeparam name="T"></typeparam>
         /// <param name="value"></param>
         /// <returns></returns>
+        [Obsolete("Use ToStringReflection Instead.")]
         public static string Detail<T>(T value = default)
-        {
-            Type type = typeof(T);
-            string detail = "";
-            detail += type.Name + "\n";
-            
-            if (type.IsValueType)
-            {
-                detail += "ValueType";
-            }
-            else
-            {
-                {
-                    detail += "Static:   \n";
-                    detail += DetailFieldProp(type, "    ", BindingFlags.Public | BindingFlags.Static);
-                }
-
-                if (value !=  null)
-                {
-                    detail += "       \n";
-                    detail += "Instance:   \n";
-                    detail += DetailFieldProp(type, "    ", BindingFlags.Public | BindingFlags.Instance);
-                }
-            }
-            return detail;
-
-            static string DetailFieldProp(Type type, string retract, BindingFlags binding)
-            {
-                string detail = "";
-                detail += retract + "Field:   \n";
-                var fields = type.GetFields(binding);
-
-                foreach (var item in fields)
-                {
-                    detail += retract + "    " + item.Name + "  :  " + item.GetValue(null)?.ToString() + "\n";
-                }
-
-                detail += retract + "Propertie:   \n";
-                var props = type.GetProperties(binding);
-
-                foreach (var item in props)
-                {
-                    detail += retract + "    " + item.Name + ":    " + item.GetValue(null)?.ToString() + "\n";
-                }
-
-                return detail;
-            }
-        }
+            => value.ToStringReflection();
     }
 }
---
+++ Megumin.Explosion/Megumin/Class/Utility.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Megumin
+{
+    /// <summary>
+    /// 工具
+    /// </summary>
+    /// <remarks>MeguminUtility名字太长,即时Utility和别的命名空间Utility冲突,使用全名称限定就可以了.</remarks>
+    public static class Utility
+    {
+
+        /// <summary>
+        /// 通过反射获得属性值字符串
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToStringReflection<T>(T value = default)
+        {
+            Type type = typeof(T);
+            string detail = "";
+            if (value.TryGetName(out var instanName))
+            {
+                detail += $"{type.Name }    {instanName}\n";
+            }
+            else
+            {
+                detail += type.Name + "\n";
+            }
+
+
+            if (type.IsValueType)
+            {
+                detail += "ValueType";
+            }
+            else
+            {
+                detail += "       \n";
+                {
+                    detail += "Static:   \n";
+                    detail += ToStringReflectionFieldProperties(type, "    ", BindingFlags.Public | BindingFlags.Static, value);
+                }
+
+                if (value != null)
+                {
+                    detail += "       \n";
+                    detail += "Instance:   \n";
+                    detail += ToStringReflectionFieldProperties(type, "    ", BindingFlags.Public | BindingFlags.Instance, value);
+                }
+            }
+            return detail;
+        }
+
+        internal static bool TryGetName<T>(this T value, out string name)
+        {
+            name = null;
+            //try
+            //{
+            //    dynamic d = value;
+            //    name = d.Name;
+            //    return true;
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //if (string.IsNullOrEmpty(name))
+            //{
+            //    try
+            //    {
+            //        dynamic d = value;
+            //        name = d.name;
+            //        return true;
+            //    }
+            //    catch (Exception)
+            //    {
+            //    }
+            //}
+            return false;
+        }
+
+        public static string ToStringReflectionFieldProperties<T>(Type type, string retract, BindingFlags binding, T value)
+        {
+            string detail = "";
+            detail += retract + "Field:   \n";
+            var fields = type.GetFields(binding);
+
+            foreach (var item in fields)
+            {
+                detail += retract + "    " + item.Name + "  :  " + item.GetValue(value)?.ToString() + "\n";
+            }
+
+            detail += retract + "Propertie:   \n";
+            var props = type.GetProperties(binding);
+
+            foreach (var item in props)
+            {
+                detail += retract + "    " + item.Name + ":    " + item.GetValue(value)?.ToString() + "\n";
+            }
+
+            return detail;
+        }
+    }
+
+    /// <summary>
+    /// 因为由扩展方法和静态方法重名
+    /// </summary>
+    public static class Utility2
+    {
+        /// <summary>
+        /// 通过反射获得属性值字符串,一般用于打印到控制台.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToStringReflection<T>(this T value)
+        {
+            return Utility.ToStringReflection(value);
+        }
+    }
+}
