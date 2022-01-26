039bc38773aeaed52efb7ccfcd00bb54494164ed
云却 <479813005@qq.com>
2022-01-26 17:13:18 +0800

    增加命名空间

--- Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/PackageWizard.cs
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Editor/Scripts/PackageWizard.cs
@@ -4,157 +4,159 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-public class PackageWizard : EditorWindow
+namespace Megumin
 {
-    const float k_WindowWidth = 600f;
-    const float k_MaxWindowHeight = 800f;
-    const float k_ScreenSizeWindowBuffer = 50f;
-
-    [MenuItem("Tools/Package Wizard...")]
-    static void CreateWindow()
+    public class PackageWizard : EditorWindow
     {
-        PackageWizard wizard = GetWindow<PackageWizard>(true, "Package Wizard", true);
+        const float k_WindowWidth = 600f;
+        const float k_MaxWindowHeight = 800f;
+        const float k_ScreenSizeWindowBuffer = 50f;
 
-        wizard.minSize = new Vector2(k_WindowWidth, 400);
-
-        Vector2 position = Vector2.zero;
-        SceneView sceneView = SceneView.lastActiveSceneView;
-
-        if (sceneView != null)
+        [MenuItem("Tools/Package Wizard...")]
+        static void CreateWindow()
         {
-            position = new Vector2(sceneView.position.x, sceneView.position.y);
-        }
+            PackageWizard wizard = GetWindow<PackageWizard>(true, "Package Wizard", true);
 
-        wizard.position = new Rect(position.x + k_ScreenSizeWindowBuffer, position.y + k_ScreenSizeWindowBuffer, k_WindowWidth, Mathf.Min(Screen.currentResolution.height - k_ScreenSizeWindowBuffer, k_MaxWindowHeight));
-        wizard.Show();
-    }
+            wizard.minSize = new Vector2(k_WindowWidth, 400);
 
-    readonly GUIContent m_NameContent = new GUIContent("PackageName");
-    string InputPackageName = "TestPackage";
-    bool CreateRuntimeAsmdef = true;
-    bool CreateEditorAsmdef = false;
-    string NameExtension = "com.megumin";
-    bool AutoFullName = true;
-    string FullName = null;
-    bool CreateReadme = true;
-    bool CreateChangeLog = true;
+            Vector2 position = Vector2.zero;
+            SceneView sceneView = SceneView.lastActiveSceneView;
 
-    void OnGUI()
-    {
-        InputPackageName = EditorGUILayout.TextField(m_NameContent, InputPackageName);
-        var path = Path.GetFullPath($"{MeguminUtility4Unity.PackagesPath}/{InputPackageName}");
-
-        CreateRuntimeAsmdef = EditorGUILayout.Toggle("CreateRuntimeAsmdef", CreateRuntimeAsmdef);
-        CreateEditorAsmdef = EditorGUILayout.Toggle("CreateEditorAsmdef", CreateEditorAsmdef);
-
-        EditorGUILayout.Separator();
-        EditorGUILayout.HelpBox("NameExtension用于包全名前缀,自动变为小写.", MessageType.Info);
-        if (EditorGUILayout.LinkButton("Start with <domain-name-extension>.<company-name> (for example, \"com.example\" \"net.example\")"))
-        {
-            Application.OpenURL("https://docs.unity3d.com/2020.3/Documentation/Manual/cus-naming.html");
-        }
-        NameExtension = EditorGUILayout.TextField("NameExtension", NameExtension);
-
-        EditorGUILayout.Separator();
-        EditorGUILayout.HelpBox("FullName用于创建asmdef程序集文件名,name和rootNamespace。", MessageType.Info);
-        AutoFullName = EditorGUILayout.Toggle("AutoFullName", AutoFullName);
-        if (AutoFullName)
-        {
-            FullName = $"Megumin.GameFramework.{InputPackageName}";
-            EditorGUILayout.LabelField("FullName", FullName);
-        }
-        else
-        {
-            FullName = EditorGUILayout.TextField("FullName", FullName);
-        }
-
-        EditorGUILayout.Separator();
-        CreateReadme = EditorGUILayout.Toggle(nameof(CreateReadme), CreateReadme);
-        CreateChangeLog = EditorGUILayout.Toggle(nameof(CreateChangeLog), CreateChangeLog);
-
-        EditorGUILayout.Separator();
-        EditorGUILayout.BeginHorizontal();
-
-        if (GUILayout.Button("Delete", GUILayout.Width(60f)))
-        {
-            if (Directory.Exists(path))
+            if (sceneView != null)
             {
-                if (EditorUtility.DisplayDialog("删除确认",
-                                                "确定要删除本地包么?\n操作不可恢复,请做好备份或使用版本工具.",
-                                                "确定", "取消"))
-                {
-                    Directory.Delete(path, true);
-                    RefreshAsset();
-                }
+                position = new Vector2(sceneView.position.x, sceneView.position.y);
             }
+
+            wizard.position = new Rect(position.x + k_ScreenSizeWindowBuffer, position.y + k_ScreenSizeWindowBuffer, k_WindowWidth, Mathf.Min(Screen.currentResolution.height - k_ScreenSizeWindowBuffer, k_MaxWindowHeight));
+            wizard.Show();
         }
 
-        GUILayout.FlexibleSpace();
-        GUILayout.Space(5);
+        readonly GUIContent m_NameContent = new GUIContent("PackageName");
+        string InputPackageName = "TestPackage";
+        bool CreateRuntimeAsmdef = true;
+        bool CreateEditorAsmdef = false;
+        string NameExtension = "com.megumin";
+        bool AutoFullName = true;
+        string FullName = null;
+        bool CreateReadme = true;
+        bool CreateChangeLog = true;
 
-        if (GUILayout.Button("Create", GUILayout.Width(60f)))
+        void OnGUI()
         {
-            CreatePackageFolder(path);
-            RefreshAsset();
-            Close();
-        }
+            InputPackageName = EditorGUILayout.TextField(m_NameContent, InputPackageName);
+            var path = Path.GetFullPath($"{MeguminUtility4Unity.PackagesPath}/{InputPackageName}");
 
-        EditorGUILayout.EndHorizontal();
-        GUILayout.Space(5);
-        //通过打开文件夹除非工程刷新
-        //EditorGUILayout.HelpBox("创建完成后Editor切换到后台，再切换回来，" +
-        //    "触发package导入，没有找到如何自动导入。", MessageType.Info);
-    }
+            CreateRuntimeAsmdef = EditorGUILayout.Toggle("CreateRuntimeAsmdef", CreateRuntimeAsmdef);
+            CreateEditorAsmdef = EditorGUILayout.Toggle("CreateEditorAsmdef", CreateEditorAsmdef);
 
-    private static void RefreshAsset()
-    {
-        var process = System.Diagnostics.Process.Start(MeguminUtility4Unity.PackagesPath);
-    }
-
-    private void CreatePackageFolder(string path)
-    {
-        if (string.IsNullOrEmpty(InputPackageName))
-        {
-            Debug.LogError($"包名不能为空");
-            return;
-        }
-
-        if (Directory.Exists(path))
-        {
-            Debug.LogError($"包已存在");
-        }
-        else
-        {
-            Directory.CreateDirectory(path);
-            Directory.CreateDirectory(path + "/Editor");
-            Directory.CreateDirectory(path + "/Runtime");
-            Directory.CreateDirectory(path + "/Tests");
-
-            CreatePackageInfoFile(path);
-
-            CreateRuntimeAsmdefFile(path);
-        }
-
-        CreateEditorAsmdefFile(path);
-
-        CreateReadmeFile(path);
-
-        CreateChangeLogFile(path);
-    }
-
-    private void CreateChangeLogFile(string path)
-    {
-        if (CreateChangeLog)
-        {
-            var filepath = Path.Combine(path, "CHANGELOG.md");
-            if (File.Exists(filepath))
+            EditorGUILayout.Separator();
+            EditorGUILayout.HelpBox("NameExtension用于包全名前缀,自动变为小写.", MessageType.Info);
+            if (EditorGUILayout.LinkButton("Start with <domain-name-extension>.<company-name> (for example, \"com.example\" \"net.example\")"))
             {
-                Debug.LogWarning("CHANGELOG.md exists");
+                Application.OpenURL("https://docs.unity3d.com/2020.3/Documentation/Manual/cus-naming.html");
+            }
+            NameExtension = EditorGUILayout.TextField("NameExtension", NameExtension);
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.HelpBox("FullName用于创建asmdef程序集文件名,name和rootNamespace。", MessageType.Info);
+            AutoFullName = EditorGUILayout.Toggle("AutoFullName", AutoFullName);
+            if (AutoFullName)
+            {
+                FullName = $"Megumin.GameFramework.{InputPackageName}";
+                EditorGUILayout.LabelField("FullName", FullName);
             }
             else
             {
-                var dataStr = System.DateTimeOffset.Now.ToString("yyyy-MM-dd");
-                string fileStr =
+                FullName = EditorGUILayout.TextField("FullName", FullName);
+            }
+
+            EditorGUILayout.Separator();
+            CreateReadme = EditorGUILayout.Toggle(nameof(CreateReadme), CreateReadme);
+            CreateChangeLog = EditorGUILayout.Toggle(nameof(CreateChangeLog), CreateChangeLog);
+
+            EditorGUILayout.Separator();
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Delete", GUILayout.Width(60f)))
+            {
+                if (Directory.Exists(path))
+                {
+                    if (EditorUtility.DisplayDialog("删除确认",
+                                                    "确定要删除本地包么?\n操作不可恢复,请做好备份或使用版本工具.",
+                                                    "确定", "取消"))
+                    {
+                        Directory.Delete(path, true);
+                        RefreshAsset();
+                    }
+                }
+            }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.Space(5);
+
+            if (GUILayout.Button("Create", GUILayout.Width(60f)))
+            {
+                CreatePackageFolder(path);
+                RefreshAsset();
+                Close();
+            }
+
+            EditorGUILayout.EndHorizontal();
+            GUILayout.Space(5);
+            //通过打开文件夹除非工程刷新
+            //EditorGUILayout.HelpBox("创建完成后Editor切换到后台，再切换回来，" +
+            //    "触发package导入，没有找到如何自动导入。", MessageType.Info);
+        }
+
+        private static void RefreshAsset()
+        {
+            var process = System.Diagnostics.Process.Start(MeguminUtility4Unity.PackagesPath);
+        }
+
+        private void CreatePackageFolder(string path)
+        {
+            if (string.IsNullOrEmpty(InputPackageName))
+            {
+                Debug.LogError($"包名不能为空");
+                return;
+            }
+
+            if (Directory.Exists(path))
+            {
+                Debug.LogError($"包已存在");
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+                Directory.CreateDirectory(path + "/Editor");
+                Directory.CreateDirectory(path + "/Runtime");
+                Directory.CreateDirectory(path + "/Tests");
+
+                CreatePackageInfoFile(path);
+
+                CreateRuntimeAsmdefFile(path);
+            }
+
+            CreateEditorAsmdefFile(path);
+
+            CreateReadmeFile(path);
+
+            CreateChangeLogFile(path);
+        }
+
+        private void CreateChangeLogFile(string path)
+        {
+            if (CreateChangeLog)
+            {
+                var filepath = Path.Combine(path, "CHANGELOG.md");
+                if (File.Exists(filepath))
+                {
+                    Debug.LogWarning("CHANGELOG.md exists");
+                }
+                else
+                {
+                    var dataStr = System.DateTimeOffset.Now.ToString("yyyy-MM-dd");
+                    string fileStr =
 $@"# Changelog
 All notable changes to this project will be documented in this file.
 
@@ -180,37 +182,36 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 PackageWizard Fast Created.
 
 ";
-                File.WriteAllText(filepath, fileStr);
+                    File.WriteAllText(filepath, fileStr);
+                }
             }
         }
-    }
 
-    private void CreateReadmeFile(string path)
-    {
-        if (CreateReadme)
+        private void CreateReadmeFile(string path)
         {
-            var filepath = Path.Combine(path, "README.md");
-            if (File.Exists(filepath))
+            if (CreateReadme)
             {
-                Debug.LogWarning("README.md exists");
-            }
-            else
-            {
-                string readmeStr =
+                var filepath = Path.Combine(path, "README.md");
+                if (File.Exists(filepath))
+                {
+                    Debug.LogWarning("README.md exists");
+                }
+                else
+                {
+                    string readmeStr =
 $@"# {InputPackageName}
 PackageWizard Fast Created.
 
 ";
-                File.WriteAllText(filepath, readmeStr);
+                    File.WriteAllText(filepath, readmeStr);
+                }
             }
         }
-    }
 
-    private void CreatePackageInfoFile(string path)
-    {
-        string packageInfo =
-$@"
-{{
+        private void CreatePackageInfoFile(string path)
+        {
+            string packageInfo =
+$@"{{
     ""name"": ""{NameExtension.ToLower()}.{InputPackageName.ToLower()}"",
     ""displayName"": ""{InputPackageName}"",
     
@@ -242,14 +243,14 @@ $@"
 
 ";
 
-        File.WriteAllText(path + "/package.json", packageInfo);
-    }
+            File.WriteAllText(path + "/package.json", packageInfo);
+        }
 
-    private void CreateRuntimeAsmdefFile(string path)
-    {
-        if (CreateRuntimeAsmdef)
+        private void CreateRuntimeAsmdefFile(string path)
         {
-            string runtimeasmdef =
+            if (CreateRuntimeAsmdef)
+            {
+                string runtimeasmdef =
 $@"{{
     ""name"": ""{FullName}"",
     ""rootNamespace"": ""{FullName}"",
@@ -265,52 +266,52 @@ $@"{{
     ""noEngineReferences"": false
 }}";
 
-            File.WriteAllText(path + "/Runtime" + $"/{FullName}.asmdef", runtimeasmdef);
+                File.WriteAllText(path + "/Runtime" + $"/{FullName}.asmdef", runtimeasmdef);
 
-            //无法找到构造函数
-            //AssemblyDefinitionAsset assembly =
-            //    System.Activator.CreateInstance(typeof(AssemblyDefinitionAsset),
-            //    BindingFlags.NonPublic| BindingFlags.Instance,
-            //    runtimeasmdef)
-            //    as AssemblyDefinitionAsset;
+                //无法找到构造函数
+                //AssemblyDefinitionAsset assembly =
+                //    System.Activator.CreateInstance(typeof(AssemblyDefinitionAsset),
+                //    BindingFlags.NonPublic| BindingFlags.Instance,
+                //    runtimeasmdef)
+                //    as AssemblyDefinitionAsset;
 
-            //CreateAsset无法创建.asmdef
-            //TextAsset asset = new TextAsset(runtimeasmdef);
-            //AssetDatabase.CreateAsset(asset, path + "/Runtime" + $"/{InputPackageName}.asmdef");
-            //AssetDatabase.Refresh();
+                //CreateAsset无法创建.asmdef
+                //TextAsset asset = new TextAsset(runtimeasmdef);
+                //AssetDatabase.CreateAsset(asset, path + "/Runtime" + $"/{InputPackageName}.asmdef");
+                //AssetDatabase.Refresh();
 
-            //无法取得guid
-            //AssetDatabase.ImportAsset(path + "/Runtime" + $"/{InputPackageName}.asmdef");
-            //var guid = AssetDatabase.GUIDFromAssetPath(path + "/Runtime" + $"/{InputPackageName}.asmdef");
-            //Debug.Log(guid);
+                //无法取得guid
+                //AssetDatabase.ImportAsset(path + "/Runtime" + $"/{InputPackageName}.asmdef");
+                //var guid = AssetDatabase.GUIDFromAssetPath(path + "/Runtime" + $"/{InputPackageName}.asmdef");
+                //Debug.Log(guid);
+            }
         }
-    }
 
-    private void CreateEditorAsmdefFile(string path)
-    {
-        if (CreateEditorAsmdef)
+        private void CreateEditorAsmdefFile(string path)
         {
-            var dir = path + "/Editor";
-            if (!Directory.Exists(dir))
+            if (CreateEditorAsmdef)
             {
-                Directory.CreateDirectory(path);
-            }
+                var dir = path + "/Editor";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(path);
+                }
 
-            var editorNamespace = FullName;
-            if (!string.IsNullOrEmpty(editorNamespace))
-            {
-                editorNamespace = $"{editorNamespace}.Editor";
-            }
+                var editorNamespace = FullName;
+                if (!string.IsNullOrEmpty(editorNamespace))
+                {
+                    editorNamespace = $"{editorNamespace}.Editor";
+                }
 
-            var filePath = path + "/Editor" + $"/{editorNamespace}.asmdef";
+                var filePath = path + "/Editor" + $"/{editorNamespace}.asmdef";
 
-            if (File.Exists(filePath))
-            {
-                Debug.LogWarning($"{editorNamespace}.asmdef exists");
-            }
-            else
-            {
-                string editorasmdef =
+                if (File.Exists(filePath))
+                {
+                    Debug.LogWarning($"{editorNamespace}.asmdef exists");
+                }
+                else
+                {
+                    string editorasmdef =
 $@"{{
     ""name"": ""{editorNamespace}"",
     ""rootNamespace"": ""{editorNamespace}"",
@@ -330,10 +331,12 @@ $@"{{
     ""noEngineReferences"": false
 }}";
 
-                File.WriteAllText(filePath, editorasmdef);
+                    File.WriteAllText(filePath, editorasmdef);
+                }
             }
         }
     }
+
 }
 
 #endif
