4feae315a87d16091665eed94220cccf4602b870
云却 <479813005@qq.com>
2022-02-26 13:47:51 +0800

    增加主线程切换器

---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/UnityMainThreadSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace Megumin
+{
+    [DefaultExecutionOrder(int.MinValue)]
+    public class UnityMainThreadSwitcher : MonoBehaviour
+    {
+        /// <summary>
+        /// 一样是每帧一次，不如用Update。
+        /// </summary>
+        SynchronizationContext context;
+
+        private void Awake()
+        {
+            context = SynchronizationContext.Current;
+            context.Post(MyCallBack, null);
+        }
+
+        private void MyCallBack(object state)
+        {
+            DefaultThreadSwitcher.Tick();
+            Debug.Log($"{Time.frameCount} -- {Utility.ToStringReflection<Time>()}");
+            context.Post(MyCallBack, null);
+        }
+    }
+
+}
