616942225b631b1466c07290e1eaecbb350f34bc
云却 <479813005@qq.com>
2022-03-25 20:31:28 +0800

    主线程切换器

---
+++ Megumin.Explosion/Threading/SynchronizationContextExtension.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace System.Threading
+{
+    public static class SynchronizationContextExtension_F196DAE75A234F72A3AC998F76A66F1A
+    {
+        public struct Awaitable
+        {
+            bool IsCompletedAwaitable;
+            internal SynchronizationContext Context;
+
+            public static readonly Awaitable CompletedTask = new Awaitable() { IsCompletedAwaitable = true };
+
+            public struct Awaiter : ICriticalNotifyCompletion, INotifyCompletion
+            {
+                private Awaitable awaitable;
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public Awaiter(Awaitable awaitable)
+                {
+                    this.awaitable = awaitable;
+                }
+
+                public bool IsCompleted
+                {
+                    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                    get
+                    {
+                        //永远返回false，总是强制挂起。
+                        return awaitable.IsCompletedAwaitable;
+                    }
+                }
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public void GetResult()
+                {
+                    //不必进行任何操作，此处已经是切换线程后。
+                }
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public void UnsafeOnCompleted(Action continuation)
+                {
+                    awaitable.Context.Post(state => { continuation?.Invoke(); }, null);
+                }
+
+                public void OnCompleted(Action continuation)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Awaiter GetAwaiter()
+            {
+                return new Awaiter(this);
+            }
+        }
+
+        public static Awaitable Switch(this SynchronizationContext context)
+        {
+            Awaitable awaitable = new Awaitable();
+            awaitable.Context = context;
+            return awaitable;
+        }
+    }
+}
--- Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/UnityMainThreadSwitcher.cs
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/UnityMainThreadSwitcher.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Megumin
@@ -28,4 +29,43 @@ namespace Megumin
         }
     }
 
+    public class MainThread
+    {
+        /// <summary>
+        /// 一样是每帧一次，不如用Update。
+        /// </summary>
+        static SynchronizationContext context;
+
+        public static SynchronizationContext CurrentContext => context;
+
+        [RuntimeInitializeOnLoadMethod]
+        static void InitContext()
+        {
+            context = SynchronizationContext.Current;
+        }
+
+        public static SynchronizationContextExtension_F196DAE75A234F72A3AC998F76A66F1A.Awaitable Switch()
+        {
+            if (SynchronizationContext.Current == context)
+            {
+                return SynchronizationContextExtension_F196DAE75A234F72A3AC998F76A66F1A.Awaitable.CompletedTask;
+            }
+            else
+            {
+                return context.Switch();
+            }
+        }
+
+        public static async ValueTask Switch2()
+        {
+            if (SynchronizationContext.Current == context)
+            {
+                return;
+            }
+            else
+            {
+                await context.Switch();
+            }
+        }
+    }
 }
