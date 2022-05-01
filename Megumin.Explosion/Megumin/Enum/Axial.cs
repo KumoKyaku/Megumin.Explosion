c3328dee45d710ef8df321421a0ca6a7f909e466
云却 <479813005@qq.com>
2022-05-01 12:51:16 +0800

    取得骨骼轴向

--- Megumin.Explosion/Megumin/Enum/Axial.cs
+++ Megumin.Explosion/Megumin/Enum/Axial.cs
@@ -12,8 +12,32 @@ namespace Megumin
     [Flags]
     public enum Axial
     {
+        None = 0x0,
         X = 1 << 0,
         Y = 1 << 1,
         Z = 1 << 2,
+    }
+
+    /// <summary>
+    /// 带符号的轴向
+    /// </summary>
+    public enum AxialAigned
+    {
+        None = 0x0,
+        X = 1 << 0,
+        Y = 1 << 1,
+        Z = 1 << 2,
+        /// <summary>
+        /// 负x
+        /// </summary>
+        Xn = 1 << 3,
+        /// <summary>
+        /// 负y
+        /// </summary>
+        Yn = 1 << 4,
+        /// <summary>
+        /// 负z
+        /// </summary>
+        Zn = 1 << 5,
     }
 }
---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/UnityEngineExtensionMethod/AnimatorExtension.cs
@@ -0,0 +1,82 @@
+﻿using Megumin;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimatorExtension_B0724B3FE1814EEB809D274438B254A6
+{
+    /// <summary>
+    /// 取得指定骨骼轴向
+    /// </summary>
+    /// <param name="animator"></param>
+    /// <param name="bone"></param>
+    /// <returns></returns>
+    public static AxialAigned GetAxial(this Animator animator, HumanBodyBones bone)
+    {
+        HumanBodyBones humanBoneId = bone.FirstChild();
+        var foot = animator.GetBoneTransform(humanBoneId);
+        var locol = foot.transform.localPosition;
+        return GetAxial(locol);
+    }
+
+    public static AxialAigned GetAxial(Vector3 locol)
+    {
+        AxialAigned axial = AxialAigned.None;
+        var absx = Mathf.Abs(locol.x);
+        var absy = Mathf.Abs(locol.y);
+        var absz = Mathf.Abs(locol.z);
+
+        if (absx > absy)
+        {
+            if (absx > absz)
+            {
+                if (locol.x > 0)
+                {
+                    axial = AxialAigned.X;
+                }
+                else
+                {
+                    axial = AxialAigned.Xn;
+                }
+            }
+            else
+            {
+                if (locol.z > 0)
+                {
+                    axial = AxialAigned.Z;
+                }
+                else
+                {
+                    axial = AxialAigned.Zn;
+                }
+            }
+        }
+        else
+        {
+            if (absy > absz)
+            {
+                if (locol.y > 0)
+                {
+                    axial = AxialAigned.Y;
+                }
+                else
+                {
+                    axial = AxialAigned.Yn;
+                }
+            }
+            else
+            {
+                if (locol.z > 0)
+                {
+                    axial = AxialAigned.Z;
+                }
+                else
+                {
+                    axial = AxialAigned.Zn;
+                }
+            }
+        }
+
+        return axial;
+    }
+}
