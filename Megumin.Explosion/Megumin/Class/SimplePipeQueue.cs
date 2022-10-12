73f550d83e40b73dc7bfc466670ec0059fd5ca66
云却 <479813005@qq.com>
2022-10-12 18:31:28 +0800

    曾加一个trancelistener

--- Megumin.Explosion/Megumin/Class/SimplePipeQueue.cs
+++ Megumin.Explosion/Megumin/Class/SimplePipeQueue.cs
@@ -91,6 +91,11 @@ namespace Megumin
     /// <para></para>这是个简单的实现,更复杂使用微软官方实现<see cref="System.Threading.Channels.Channel.CreateBounded{T}(int)"/>
     /// </summary>
     /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// <para/> 如果能用 while(true),就不要用递归。
+    /// <para/> 在捕捉上下文时，while(true)堆栈更少更清晰，逻辑上复合直觉，不容易爆栈。
+    /// <para/> 递归还会导致方法引用计数增加，阅读代码时制造混乱。
+    /// </remarks>
     public class QueuePipe<T> : Queue<T>, IPipe<T>
     {
         readonly object _innerLock = new object();
---
+++ Megumin.UnityPackage/Packages/megumin.explosion4unity/Runtime/Scripts/NewClass/Debug/UnityTraceListener.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Megumin
+{
+    public class UnityTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public override void Write(object o, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(o);
+            }
+            else
+            {
+                base.Write(o, category);
+            }
+        }
+
+        public override void Write(string message, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(message);
+            }
+            else
+            {
+                base.Write(message, category);
+            }
+        }
+
+        public override void WriteLine(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public override void WriteLine(object o, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(o);
+            }
+            else
+            {
+                base.WriteLine(o, category);
+            }
+        }
+
+        public override void WriteLine(string message, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(message);
+            }
+            else
+            {
+                base.WriteLine(message, category);
+            }
+        }
+
+        public override void Fail(string message, string detailMessage)
+        {
+            Debug.LogError($"{message}\n{detailMessage}");
+        }
+
+        public override void Fail(string message)
+        {
+            Debug.LogError(message);
+        }
+    }
+}
+
+
